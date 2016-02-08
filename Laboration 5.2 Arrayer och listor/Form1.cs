cf57891c3a1cc06a37f183bd2390cdd5d96909bf
Hammerek <anakin165@wp.pl>
2016-02-08 11:48:00 +0100

    Final work. Done!

--- Laboration 5.2 Arrayer och listor/Form1.cs
+++ Laboration 5.2 Arrayer och listor/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Laboration_5._2_Arrayer_och_listor
+namespace Laboration_5._2_Arrayer_och_listor //Kamil EEDAT14A
 {
     public partial class Form1 : Form
     {
@@ -17,25 +17,25 @@ namespace Laboration_5._2_Arrayer_och_listor
         {
             InitializeComponent();
         }
-        int counter = 0;
-        int[] minArray = new int[5];
+        int counter = 0; //variabel som kommer räkna antal tryck på knappen 
+        int[] minArray = new int[5]; //array som innehåller 5 tal 
         private void button1_Click(object sender, EventArgs e)
         {
-            int tal = Convert.ToInt32(textBox1.Text);
+            int tal = Convert.ToInt32(textBox1.Text); //variabel till tal som man skriver i textboxen
 
-            minArray[counter] = tal;
+            minArray[counter] = tal; //tal är arrayen med antal tryckningar 
             counter++;
 
-            if (counter == 5)
+            if (counter == 5) //om man trycker fem gånger på knappen så 
             {
-                button2.Visible = true;
-                button1.Enabled = false;
+                button2.Visible = true; //button2 visar sig 
+                button1.Enabled = false; //button1 går inte att trycka 
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            negativaTal();
+            negativaTal(); //metoder 
             positivaTal();
             nollTal();
             jamnaUddaTal();
@@ -43,58 +43,59 @@ namespace Laboration_5._2_Arrayer_och_listor
 
         private void negativaTal()
         {
-            int negativCounter = 0;
-            foreach (int times in minArray)
+            int negativCounter = 0;//räknare för negativa tal 
+            foreach (int times in minArray)//foreach går genom varje tal i arrayen och gör "if" för varje tal
             {
-                if (times < 0)
+                if (times < 0)// om varje tal i arrayen är mindre än noll 
                 {
-                    negativCounter++;
-                    richTextBox1.Text = Convert.ToString(negativCounter);
+                    negativCounter++;//antal plussas ihop
+                    richTextBox1.Text = Convert.ToString(negativCounter);//visar antal negativa tal i richtextboxen 
                 }
             }
         }
         private void positivaTal()
         {
-            int positivCounter = 0;
-            foreach (int times in minArray)
+            int positivCounter = 0; //räknare för positiva tal
+            foreach (int times in minArray)//för varje tal i arrayen 
             {
-                if (times > 0)
+                if (times >= 0)//om tal är större eller lika med noll 
                 {
-                    positivCounter++;
-                    richTextBox2.Text = Convert.ToString(positivCounter);
+                    positivCounter++;//tal plussas ihop
+                    richTextBox2.Text = Convert.ToString(positivCounter);//visar antal positiva tal i richtextboxen 
                 }
             }
         }
 
         private void nollTal()
         {
-            int nollCounter = 0;
-            foreach (int times in minArray)
+            int nollCounter = 0;//räknare för nollor
+            
+            foreach (int times in minArray)//for varje nolla i arrayen 
             {
-                if (times == 0)
+                if (minArray.Contains(0))// om tal innehåller 0 
                 {
-                    nollCounter++;
-                    richTextBox3.Text = Convert.ToString(nollCounter);
+                    nollCounter++;//tal plussas ihop
+                    richTextBox3.Text = Convert.ToString(nollCounter);//visar antal nollor i richtextboxen
                 }
             }
         }
 
         private void jamnaUddaTal()
         {
-            int jamnaCounter = 0;
-            int uddaCounter = 0;
+            int jamnaCounter = 0;//räknare för jämna tal
+            int uddaCounter = 0;//räknare för udda tal
 
-            foreach (int times in minArray)
+            foreach (int times in minArray)//för varje tal i arrayen
             {
-                if (times % 2 == 0)
+                if (times % 2 == 0)//om tal är jämnt
                 {
-                    jamnaCounter++;
-                    richTextBox4.Text = Convert.ToString(jamnaCounter);
+                    jamnaCounter++;//tal plussas ihop
+                    richTextBox4.Text = Convert.ToString(jamnaCounter);//visar antal jämna tal i richtextboxen
                 }
-                else if (times % 2 != 0)
+                else if (times % 2 != 0)//annars om talen är udda
                 {
-                    uddaCounter++;
-                    richTextBox5.Text = Convert.ToString(uddaCounter);
+                    uddaCounter++;//tal plussas ihop 
+                    richTextBox5.Text = Convert.ToString(uddaCounter);//visar antal udda tal i richtextboxen
                 }
             }
         }
